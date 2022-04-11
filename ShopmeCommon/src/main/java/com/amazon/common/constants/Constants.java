fee6cb4f65b770a8187f3a5ffe404de9ee19eae9
sjulu <julurusumanth200@gmail.com>
2022-04-11 21:56:40 +0530

    shopme commite

--- ShopmeCommon/src/main/java/com/amazon/common/constants/Constants.java
+++ ShopmeCommon/src/main/java/com/amazon/common/constants/Constants.java
@@ -8,6 +8,7 @@ public class Constants {
 		String region= System.getenv("AWS_REGION");
 		String pattern = "https://%s.s3.%s.amazonaws.com";
 		
+		
 	
 
 		S3_BASE_URI= bucketName == null ? "" : String.format(pattern, bucketName,region);
